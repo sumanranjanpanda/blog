bcdcb5e5d96ddbd585857d81274fe22735ca1f11
suman <sumanranjanpanda@hotmail.com>
2012-08-28 19:18:38 +0530

    pushing all files for blogs

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DmarieBlog::Application.config.secret_token = 'ca825acfe2c7fe78df1da60aa2e4758a3a9699e1aa7ec10763282b963b1610abe5f796c35e66172619e2f485235169b0488a5072723f4edd61df412b255e0a3f'
